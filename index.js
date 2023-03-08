18ff6b86a39615e0ae15fdcd9f1378855b8ba7e9
shafin-saiyed <103881410+shafin-saiyed@users.noreply.github.com>
2023-03-08 22:16:58 +0530

    Swagger Implementation

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-//const app = require('./routes');
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger_output.json')
 dotenv.config()
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 app.use('/', router);
+
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongoose
  .connect('mongodb://localhost:27017/FMCG_DB')
